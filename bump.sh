23277b8f241bfbe9f47075d0e2be9f73c101336d
Brandon Chong <phototix@hotmail.com>
2026-07-20 23:26:34 +0800

    bump cache-bust to v24, add landing page to bump.sh

--- bump.sh
+++ bump.sh
@@ -2,12 +2,14 @@
 set -e
 
 VERSION_FILE="$(dirname "$0")/version.txt"
-INDEX_FILE="$(dirname "$0")/dashboard.html"
+DASHBOARD_FILE="$(dirname "$0")/dashboard.html"
+LANDING_FILE="$(dirname "$0")/index.html"
 
 OLD_VER=$(cat "$VERSION_FILE")
 NEW_VER=$((OLD_VER + 1))
 
-sed -i '' "s/\\?v=$OLD_VER/?v=$NEW_VER/g" "$INDEX_FILE"
+sed -i '' "s/\\?v=$OLD_VER/?v=$NEW_VER/g" "$DASHBOARD_FILE"
+sed -i '' "s/\\?v=$OLD_VER/?v=$NEW_VER/g" "$LANDING_FILE"
 echo "$NEW_VER" > "$VERSION_FILE"
 
-echo "Bumped v$OLD_VER -> v$NEW_VER (index.html + version.txt)"
+echo "Bumped v$OLD_VER -> v$NEW_VER (dashboard.html + index.html + version.txt)"
